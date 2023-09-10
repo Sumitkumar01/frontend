d8a634f131e1a5fbe1af0fa5a32b646fe408b6e1
sumit <sk7627577@gmail.com>
2023-09-10 16:44:19 +0530

    fix error

--- src/pages/ShowBook.js
+++ src/pages/ShowBook.js
@@ -15,7 +15,8 @@ function ShowBook() {
         axios
             .get(`http://localhost:8000/books/${id}`)
             .then((response) => {
-                setBook(response.data);
+                const data = response.data.book;
+                setBook(data);
                 setLoading(false);
             })
             .catch((error) => {
@@ -31,38 +32,37 @@ function ShowBook() {
                 Show Book
             </h1>
 
-            {
-                loading ? (
-                    <Spinner />
-                ) : (
-                    <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
-                        <div className='my-4'>{book._id}
-                            <span className='test-xl mr-4 text-gray-500'>Id</span>
-                            <span>{book._id}</span>
+            {loading ? (
+                <Spinner />
+            ) : (
+                <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
+                    <div className='my-4'>
+                        <span className='test-xl mr-4 text-gray-500'>Id :</span>
+                        <span>{book._id}</span>
 
-                        </div>
-                        <div className='my-4'>
-                            <span className='test-xl mr-4 text-gray-500'>Title</span>
-                            <span>{book.title}</span>
-                        </div>
-                        <div className='my-4'>
-                            <span className='test-xl mr-4 text-gray-500'>Author</span>
-                            <span>{book.author}</span>
-                        </div>
-                        <div className='my-4'>
-                            <span className='test-xl mr-4 text-gray-500'>Publish Year</span>
-                            <span>{book.publishYear}</span>
-                        </div>
-                        <div className='my-4'>
-                            <span className='test-xl mr-4 text-gray-500'>Create Time</span>
-                            <span>{new Date(book.createdAt).toString()}</span>
-                        </div>
-                        <div className='my-4'>
-                            <span className='test-xl mr-4 text-gray-500'>Last Update Time</span>
-                            <span>{new Date(book.updateAt).toString()}</span>
-                        </div>
                     </div>
-                )
+                    <div className='my-4'>
+                        <span className='test-xl mr-4 text-gray-500'>Title</span>
+                        <span>{book.title}</span>
+                    </div>
+                    <div className='my-4'>
+                        <span className='test-xl mr-4 text-gray-500'>Author</span>
+                        <span>{book.author}</span>
+                    </div>
+                    <div className='my-4'>
+                        <span className='test-xl mr-4 text-gray-500'>Publish Year</span>
+                        <span>{book.publishYear}</span>
+                    </div>
+                    <div className='my-4'>
+                        <span className='test-xl mr-4 text-gray-500'>Create Time</span>
+                        <span>{new Date(book.createdAt).toString()}</span>
+                    </div>
+                    <div className='my-4'>
+                        <span className='test-xl mr-4 text-gray-500'>Last Update Time</span>
+                        <span>{new Date(book.updateAt).toString()}</span>
+                    </div>
+                </div>
+            )
             }
         </div>
     )
